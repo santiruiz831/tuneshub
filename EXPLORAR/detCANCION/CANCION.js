230b272e40231f89f561e09fec189e7017b9ecc2
mateomaciel <63106091+mateomaciel@users.noreply.github.com>
2020-06-12 12:18:56 -0300

    funciona todo, falta estetica

--- EXPLORAR/detCANCION/CANCION.js
+++ EXPLORAR/detCANCION/CANCION.js
@@ -18,15 +18,14 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/' + albu
        namedelS.innerHTML += `<h3 id="namesong" style="color: blueviolet; margin: auto;margin-left: 10px;">${datos.title}</h3>`
 
        let namedelAR = document.querySelector('#nameart');
-       namedelAR.innerHTML += `<h3 id="nameartista" style="color: blueviolet; margin: auto;margin-left: 10px;">${datos.artist.name}</h3>`
+       namedelAR.innerHTML += `<a href="../detARTISTA/ARTISTA.html?id=${datos.artist.id}"  style="text-decoration: none;"><p id="nameartista" style="color: blueviolet; margin: auto;margin-left: 10px;">${datos.artist.name}</p>`
 
        let dur = document.querySelector('#duracion');
        dur.innerHTML += `<h3 id="duration" style="color: blueviolet; margin: auto;margin-left: 10px;">${datos.duration}</h3>`
 
        let namedelAL = document.querySelector('#albumname');
-       namedelAL.innerHTML += `<h3 id="nameartista" style="color: blueviolet; margin: auto;margin-left: 10px;">${datos.album.title}</h3>`
+       namedelAL.innerHTML += `<a href="/EXPLORAR/detALBUM/ALBUM.html?id=${datos.album.id}"  style="text-decoration: none;color:black"><h3 id="nameartista" style="color: blueviolet; margin: auto;margin-left: 10px;">${datos.album.title}</h3>`
     })
     .catch(function(error){
         console.error(error);
     })
-    
